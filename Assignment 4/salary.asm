759f262b717170d0bfbb76b09226e51dbe2fb754
Keane <keanemind@gmail.com>
2017-12-04 01:17:49 -0600

    Display salary and bug fixes
    Still need to a fix a bug where right nodes can't be searched

--- Assignment 4/salary.asm
+++ Assignment 4/salary.asm
@@ -64,8 +64,7 @@ NOQUIT  LDI     R1,ROOT         ; R1 contains address of root
         JSR     SEARCH
         ADD     R0,R0,#0
         BRn     NFOUND
-        LEA     R0,CELERY
-        TRAP    x22
+        JSR     PRINT_NUM
         BRnzp   #2
 
 NFOUND  LEA     R0,NOPE
@@ -88,17 +87,16 @@ CHECK1  LDR     R4,R2,#0        ; R4 contains query character
 NONZ    LDR     R5,R3,#0
 
 NONZ1   ADD     R0,R4,R5
-        BRnp    CONT
+        BRnp    LEFT
         ADD     R2,R2,#1
         ADD     R3,R3,#1
         BRnzp   CHECK1
 
         ; Return the return value of whichever call was nonzero, or return -1
-CONT    LDR     R2,R1,#0        ; R2 contains left node address
-        LDR     R3,R1,#1        ; R3 contains right node address
+LEFT    LDR     R2,R1,#0        ; R2 contains left node address
 
         ADD     R1,R2,#0        ; R1 = R2
-        BRz     #5
+        BRz     #8
 
         ; Push R7
         ADD     R6,R6,#-1
@@ -111,8 +109,11 @@ CONT    LDR     R2,R1,#0        ; R2 contains left node address
         ADD     R6,R6,#1
 
         ADD     R0,R0,#0
-        BRn     #1
+        BRn     #1              ; if left subtree found professor, return it
         RET
+
+; needs R1 to be saved.. maybe instead of storing where to return in stack, store current location
+RIGHT   LDR     R3,R1,#1        ; R3 contains right node address
         ADD     R1,R3,#0        ; R1 = R3
         BRz     ENDNODE
 
@@ -139,8 +140,60 @@ ENDNODE AND     R0,R0,#0
 
 ; ===== END OF SEARCH SUBROUTINE =====
 
-CELERY  .STRINGZ "salary here"
-NOPE    .STRINGZ "not found"
+; ===== NUM TO ASCII SUBROUTINE =====
+; Subroutine that prints a positive 2's complement number on the display
+; Input Register: R0 (positive 2's complement number)
+; Output Registers: None
+; Algorithm: The subroutine keeps dividing the input number by 10. It stores 
+;            the remainder of each division in some sequential storage growing
+;            backwards. When the quotient of the division hits zero, it prints
+;            the stored digits in the reverse order and returns.
+PRINT_NUM        ST   R0, PRINT_NUM_SAVER0
+                 ST   R1, PRINT_NUM_SAVER1
+                 ST   R6, PRINT_NUM_SAVER6
+                 ST   R7, PRINT_NUM_SAVER7
+
+                 LEA  R6, PRINT_NUM_LF ; initialize the local stack pointer
+PRINT_NUM_AGAIN  JSR  DIV10            ; Extract next digit by dividing by 10
+                 ADD  R6, R6, #-1
+                 LD   R7, PRINT_NUM_HEX30
+                 ADD  R0, R0, R7       ; Convert the single digit to ASCII
+                 STR  R0, R6, #0       ; push the ASCII digit onto the stack
+                 ADD  R1, R1, #0
+                 BRz  PRINT_NUM_DONE   ; If the quotient is zero, we are ready to print
+                 ADD  R0, R1, #0
+                 BR   PRINT_NUM_AGAIN
+PRINT_NUM_DONE   ADD  R0, R6, #0
+                 TRAP x22              ; Print all the digits in the reverse order
+                 LD   R0, PRINT_NUM_SAVER0
+                 LD   R1, PRINT_NUM_SAVER1
+                 LD   R6, PRINT_NUM_SAVER6
+                 LD   R7, PRINT_NUM_SAVER7
+                 RET
+
+PRINT_NUM_STACK  .BLKW 5
+PRINT_NUM_LF     .FILL x000A  
+PRINT_NUM_NULL   .FILL x0000
+PRINT_NUM_HEX30  .FILL x0030
+PRINT_NUM_SAVER0 .BLKW 1
+PRINT_NUM_SAVER1 .BLKW 1
+PRINT_NUM_SAVER6 .BLKW 1
+PRINT_NUM_SAVER7 .BLKW 1
+
+; Subroutine for dividing a number by 10
+; Input: R0 (the dividend)
+; Outputs: R0 (the remainder), R1(the quotient)
+DIV10            AND  R1, R1, #0
+DIV10_AGAIN      ADD  R0, R0, #-10
+                 BRn  DIV10_DONE
+                 ADD  R1, R1, #1
+                 BR   DIV10_AGAIN
+DIV10_DONE       ADD  R0, R0, #10
+                 RET
+
+; ===== END OF NUM TO ASCII SUBROUTINE
+
+NOPE    .STRINGZ "No Entry"
 BASE    .FILL xFE00
 ROOT    .FILL x4000
 LINEF   .FILL x000A             ; line feed
