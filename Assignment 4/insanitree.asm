ead44cbc965b5c6ef02c1dc8163d3c31727aa3e0
Keane <keanemind@gmail.com>
2017-12-05 01:00:31 -0600

    Add a testing tree
    Not all nodes are supposed to work (such as the duplicate ones).

---
+++ Assignment 4/insanitree.asm
@@ -0,0 +1,143 @@
+        .ORIG x4000
+ROOT    .FILL NODE1
+
+NODE1   .FILL NODE2
+        .FILL NODE3
+        .FILL JO
+        .FILL #0
+
+NODE2   .FILL NODE4
+        .FILL NODE5
+        .FILL BLANK
+        .FILL #2
+
+NODE3   .FILL NODE6
+        .FILL NODE7
+        .FILL DRD
+        .FILL #3
+
+NODE4   .FILL NODE8
+        .FILL NODE9
+        .FILL BALAK
+        .FILL #4
+
+NODE5   .FILL NODE10
+        .FILL NODE11
+        .FILL ZERO
+        .FILL #5
+
+NODE6   .FILL NODE12
+        .FILL NODE13
+        .FILL ALI
+        .FILL #6
+
+NODE7   .FILL NODE14
+        .FILL NODE15
+        .FILL ALISTAR
+        .FILL #7
+
+NODE8   .FILL x0000
+        .FILL x0000
+        .FILL BALA
+        .FILL #8
+
+NODE9   .FILL NODE16
+        .FILL NODE17
+        .FILL BALA
+        .FILL #9
+
+NODE10  .FILL x0000
+        .FILL x0000
+        .FILL J
+        .FILL #10
+
+NODE11  .FILL x0000
+        .FILL NODE18
+        .FILL JOAN
+        .FILL #11
+
+NODE12  .FILL x0000
+        .FILL x0000
+        .FILL AL
+        .FILL #12
+
+NODE13  .FILL NODE19
+        .FILL x0000
+        .FILL ALIM
+        .FILL #13
+
+NODE14  .FILL NODE20
+        .FILL x0000
+        .FILL ALISTARR
+        .FILL #14
+
+NODE15  .FILL x0000
+        .FILL NODE21
+        .FILL RINGO
+        .FILL #15
+
+NODE16  .FILL NODE22
+        .FILL x0000
+        .FILL JO1
+        .FILL #16
+
+NODE17  .FILL x0000
+        .FILL NODE23
+        .FILL ANDY
+        .FILL #17
+
+NODE18  .FILL x0000
+        .FILL x0000
+        .FILL JOA
+        .FILL #18
+
+NODE19  .FILL x0000
+        .FILL x0000
+        .FILL A
+        .FILL #19
+
+NODE20  .FILL x0000
+        .FILL x0000
+        .FILL ALISTARRR
+        .FILL #20
+
+NODE21  .FILL x0000
+        .FILL x0000
+        .FILL RINGO1
+        .FILL #21
+
+NODE22  .FILL x0000
+        .FILL x0000
+        .FILL BKSPC
+        .FILL #22
+
+NODE23  .FILL x0000
+        .FILL x0000
+        .FILL REE
+        .FILL #23
+
+JO      .STRINGZ "Jo"
+BLANK   .FILL x0000
+DRD     .STRINGZ "Ananth Dodabalapur"
+BALAK   .STRINGZ "Balakumeran Balasubramanian"
+ZERO    .STRINGZ "0"
+ALI     .STRINGZ "Ali"
+ALISTAR .STRINGZ "Alistar"
+BALA    .STRINGZ "Bala"
+J       .STRINGZ "J"
+JOAN    .STRINGZ "Joan"
+AL      .STRINGZ "Al"
+ALIM    .STRINGZ "Alim"
+ALISTARR .STRINGZ "Alistarr"
+RINGO   .STRINGZ "Ringo Starr"
+JO1     .STRINGZ "Jo"
+ANDY    .STRINGZ "Andy"
+JOA     .STRINGZ "Joa"
+A       .STRINGZ "A"
+ALISTARRR .STRINGZ "Alistarrr"
+RINGO1  .STRINGZ "Ringo Starrr"
+BKSPC   .FILL x0008
+        .FILL x0000
+REE     .STRINGZ "REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
+
+        .END
