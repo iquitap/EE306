241417a337f08f1a3f71e00204ab951db0e9b12f
Keane <keanemind@gmail.com>
2017-12-10 13:46:13 -0600

    Add last part of user_program

--- Assignment 5/user_program.asm
+++ Assignment 5/user_program.asm
@@ -16,6 +16,14 @@
         NOT     R0,R0           ; R0 = KBSR OR IE_BIT
         STI     R0,KBSR
 
+        ; Spam the prompt message forever
+LOOP    LEA     R0,MSG
+        TRAP    x22
+        LD      R0,LF
+        TRAP    x21
+        JSR     DELAY
+        BRnzp   LOOP
+
 ; ===== DELAY SUBROUTINE =====
 DELAY   ST      R1,SaveR1
         LD      R1,COUNT
@@ -32,3 +40,6 @@ TABLE   .FILL x0100
 KEY_INT .FILL x0080
 KBSR    .FILL xFE00
 IE_BIT  .FILL x4000
+MSG     .STRINGZ "Input a capital letter from the English alphabet:"
+LF      .FILL x000A
+        .END
